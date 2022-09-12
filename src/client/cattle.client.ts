f0e0eb89569e4a4685e1164c08ba9587d86b3ce6
Beker-Dev <joaopedro.beker@hotmail.com>
2022-09-12 19:53:26 -0300

    cattle_farm_clients

---
+++ src/client/cattle.client.ts
@@ -0,0 +1,108 @@
+import { Cattle } from "@/model/cattle.model";
+import axios, { AxiosInstance } from "axios";
+import { PageRequest } from "@/model/page/page-request";
+import { PageResponse } from "@/model/page/page-response";
+export class ConvenioClient {
+
+    private axiosClient: AxiosInstance;
+
+    constructor() {
+        this.axiosClient = axios.create({
+            baseURL: 'http://localhost:8085/api/cattle',
+            headers: {'Content-type' : 'application/json'}
+        });
+    }
+
+    public async findById(cattle: Cattle): Promise<Cattle> {
+        try {
+            return (await this.axiosClient.get<Cattle>(`/${cattle.id}`)).data
+        } catch (error:any) {
+            return Promise.reject()
+        }
+    }
+
+    
+    public async findByEarring(cattle: Cattle): Promise<Cattle> {
+        try {
+            return (await this.axiosClient.get<Cattle>(`/earring/${cattle.earring}`)).data
+        } catch (error:any) {
+            return Promise.reject()
+        }
+    }
+    
+    public async findByFarm(cattle: Cattle): Promise<Cattle> {
+        try {
+            return (await this.axiosClient.get<Cattle>(`/farm/${cattle.farm.id}`)).data
+        } catch (error:any) {
+            return Promise.reject()
+        }
+    }
+    
+    public async findBySpecie(cattle: Cattle): Promise<Cattle> {
+        try {
+            return (await this.axiosClient.get<Cattle>(`/specie/${cattle.specie.id}`)).data
+        } catch (error:any) {
+            return Promise.reject()
+        }
+    }
+    
+    public async findChildren(cattle: Cattle): Promise<Cattle> {
+        try {
+            return (await this.axiosClient.get<Cattle>(`/children/${cattle.earring}`)).data
+        } catch (error:any) {
+            return Promise.reject()
+        }
+    }
+
+    public async findByParents(cattle: Cattle): Promise<Cattle> {
+        try {
+            return (await this.axiosClient.get<Cattle>(`/parents/${cattle.earring}`)).data
+        } catch (error:any) {
+            return Promise.reject()
+        }
+    }
+
+  	public async findAll(pageRequest : PageRequest): Promise<PageResponse<Cattle>> {
+		try {
+			
+			let requestPath = ''
+			
+			requestPath += `?page=${pageRequest.currentPage}`
+			requestPath += `&size=${pageRequest.pageSize}`
+			requestPath += `&sort=${pageRequest.sortField === undefined 
+				? '' : pageRequest.sortField},${pageRequest.direction}`
+			
+			return (await this.axiosClient.get<PageResponse<Cattle>>(requestPath, 
+				{ 
+					params: { filtros: pageRequest.filter } 
+				}
+			)).data
+		} catch (error:any) { 
+			return Promise.reject(error.response) 
+		}
+  	}
+
+	public async save(cattle: Cattle): Promise<void> {
+		try {
+			return (await this.axiosClient.post('/', cattle))
+		} catch (error:any) {
+			return Promise.reject(error.response)
+		}
+	}
+
+	public async update(cattle: Cattle): Promise<void> {
+		try {
+			return (await this.axiosClient.put(`/${cattle.id}`, cattle)).data
+		} catch (error:any) {
+			return Promise.reject(error.response)
+		}
+	}
+
+	public async disable(cattle: Cattle): Promise<void> {
+		try {
+			return (await this.axiosClient.put(`/disable/${cattle.id}`, cattle)).data
+		} catch (error:any) {
+			return Promise.reject(error.response)
+		}
+	}
+}
---
+++ src/client/farm.client.ts
@@ -0,0 +1,83 @@
+import { Farm } from "@/model/farm.model";
+import axios, { AxiosInstance } from "axios";
+import { PageRequest } from "@/model/page/page-request";
+import { PageResponse } from "@/model/page/page-response";
+export class ConvenioClient {
+
+    private axiosClient: AxiosInstance;
+
+    constructor() {
+        this.axiosClient = axios.create({
+            baseURL: 'http://localhost:8085/api/farm',
+            headers: {'Content-type' : 'application/json'}
+        });
+    }
+
+    public async findById(farm: Farm): Promise<Farm> {
+        try {
+            return (await this.axiosClient.get<Farm>(`/${farm.id}`)).data
+        } catch (error:any) {
+            return Promise.reject()
+        }
+    }
+
+    public async findByAddress(farm: Farm): Promise<Farm> {
+        try {
+            return (await this.axiosClient.get<Farm>(`/address/${farm.address}`)).data
+        } catch (error:any) {
+            return Promise.reject()
+        }
+    }
+
+    public async findByName(farm: Farm): Promise<Farm> {
+        try {
+            return (await this.axiosClient.get<Farm>(`/name/${farm.name}`)).data
+        } catch (error:any) {
+            return Promise.reject()
+        }
+    }
+
+  	public async findAll(pageRequest : PageRequest): Promise<PageResponse<Farm>> {
+		try {
+			
+			let requestPath = ''
+			
+			requestPath += `?page=${pageRequest.currentPage}`
+			requestPath += `&size=${pageRequest.pageSize}`
+			requestPath += `&sort=${pageRequest.sortField === undefined 
+				? '' : pageRequest.sortField},${pageRequest.direction}`
+			
+			return (await this.axiosClient.get<PageResponse<Farm>>(requestPath, 
+				{ 
+					params: { filtros: pageRequest.filter } 
+				}
+			)).data
+		} catch (error:any) { 
+			return Promise.reject(error.response) 
+		}
+  	}
+
+	public async save(farm: Farm): Promise<void> {
+		try {
+			return (await this.axiosClient.post('/', farm))
+		} catch (error:any) {
+			return Promise.reject(error.response)
+		}
+	}
+
+	public async update(farm: Farm): Promise<void> {
+		try {
+			return (await this.axiosClient.put(`/${farm.id}`, farm)).data
+		} catch (error:any) {
+			return Promise.reject(error.response)
+		}
+	}
+
+	public async disable(farm: Farm): Promise<void> {
+		try {
+			return (await this.axiosClient.put(`/disable/${farm.id}`, farm)).data
+		} catch (error:any) {
+			return Promise.reject(error.response)
+		}
+	}
+}
